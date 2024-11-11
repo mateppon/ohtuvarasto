732a372cceae15ea450f922ea64470139e6f43d2
Margit Tepponen <margit.tepponen@helsinki.fi>
2024-11-11 21:48:40 +0200

    Fix lint errors

--- src/index.py
+++ src/index.py
@@ -5,53 +5,53 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
-
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
-
-    print("Mehu setterit:")
-    print("Lisätään 50.7")
+    print(
+        f"Luonnin jälkeen:\n"
+        f"Mehuvarasto: {mehua}\n"
+        f"Olutvarasto: {olutta}\n"
+        f"Olut getterit:\n"
+        f"saldo = {olutta.saldo}\n"
+        f"tilavuus = {olutta.tilavuus}\n"
+        f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}\n"
+        f"Mehu setterit:\n"
+        f"Lisätään 50.7\n"
+        )
     mehua.lisaa_varastoon(50.7)
-    print(f"Mehuvarasto: {mehua}")
-    print("Otetaan 3.14")
+    print(
+        f"Mehuvarasto: {mehua}\n"
+        f"Otetaan 3.14"
+        )
     mehua.ota_varastosta(3.14)
-    print(f"Mehuvarasto: {mehua}")
-
-    print("Virhetilanteita:")
-    print("Varasto(-100.0);")
-    huono = Varasto(-100.0)
-    print(huono)
-
-    print("Varasto(100.0, -50.7)")
-    huono = Varasto(100.0, -50.7)
-    print(huono)
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.lisaa_varastoon(1000.0)")
+    print(
+        f"Mehuvarasto: {mehua}\n"
+        f"Virhetilanteita:\n"
+        f"Varasto(-100.0);"
+        )
+    print(
+        f"{Varasto(-100.0)}\n"
+        f"Varasto(100.0, -50.7)\n")
+    print(
+        f"{Varasto(100.0, -50.7)}\n"
+        f"Olutvarasto: {olutta}\n"
+        f"olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
-    print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.lisaa_varastoon(-666.0)")
+    print(
+        f"Olutvarasto: {olutta}\n"
+        f"Mehuvarasto: {mehua}\n"
+        f"mehua.lisaa_varastoon(-666.0)"
+        )
     mehua.lisaa_varastoon(-666.0)
-    print(f"Mehuvarasto: {mehua}")
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.ota_varastosta(1000.0)")
-    saatiin = olutta.ota_varastosta(1000.0)
-    print(f"saatiin {saatiin}")
-    print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.otaVarastosta(-32.9)")
-    saatiin = mehua.ota_varastosta(-32.9)
-    print(f"saatiin {saatiin}")
-    print(f"Mehuvarasto: {mehua}")
+    print(
+        f"Mehuvarasto: {mehua}\n"
+        f"Olutvarasto: {olutta}\n"
+        f"olutta.ota_varastosta(1000.0\n"
+        f"saatiin {olutta.ota_varastosta(1000.0)}\n"
+        f"Olutvarasto: {olutta}\n"
+        f"Mehuvarasto: {mehua}\n"
+        f"mehua.otaVarastosta(-32.9)\n"
+        f"saatiin {mehua.ota_varastosta(-32.9)}\n"
+        f"Mehuvarasto: {mehua}"
+        )
 
 
 if __name__ == "__main__":
